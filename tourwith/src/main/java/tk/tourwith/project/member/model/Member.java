da72166b9ae0ae11e5e02f8fd678fcaeabcc3878
LisaPark <lissapark91@gmail.com>
2018-02-12 15:56:55 +0900

    mapper 오류 수정 및 member model 내용 추가

--- tourwith/src/main/java/tk/tourwith/project/member/model/Member.java
+++ tourwith/src/main/java/tk/tourwith/project/member/model/Member.java
@@ -1,5 +1,18 @@
 package tk.tourwith.project.member.model;
 
 public class Member {
+	
+	
+	private String mb_no;	
+	private String nick	;
+	private String mb_grad	;
+	private String fb_id	;
+	private String fb_tkn	;
+	private String reg_de	;
+	private String upd_de	;
+	private String del_yn	;
+	private String gender	;
+	private String age	;
+	private String tour_cnt ;
 
 }
