5620e4dc21f5ae31f8b9d3b2993bfda0a45e8ca5
LisaPark <lissapark91@gmail.com>
2018-02-13 10:20:13 +0900

    설정 파일 수정 및 멤버 base 클래스 추가

--- tourwith/src/main/java/tk/tourwith/project/member/mapper/MemberMapper.java
+++ tourwith/src/main/java/tk/tourwith/project/member/mapper/MemberMapper.java
@@ -1,5 +1,23 @@
 package tk.tourwith.project.member.mapper;
 
-public interface MemberMapper {
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import tk.tourwith.project.member.model.Member;
+
+@Mapper
+public interface MemberMapper {
+	
+	public List<Member> selectMemberList(Map<String, Object> paramMap) throws Exception;
+	
+	public int insertMember(Member member) throws Exception;
+	
+	public Member selectMemberByPK(String mb_no) throws Exception;
+
+	public Member selectMemberById(String fb_id) throws Exception;
+	
+	public int deleteMember(String mb_no) throws Exception;
+	
 }
--- tourwith/src/main/java/tk/tourwith/project/member/service/MemberService.java
+++ tourwith/src/main/java/tk/tourwith/project/member/service/MemberService.java
@@ -1,5 +1,23 @@
 package tk.tourwith.project.member.service;
 
-public class MemberService {
+import java.util.List;
+import java.util.Map;
 
+import tk.tourwith.project.member.model.Member;
+
+public interface MemberService {
+
+public List<Member> selectMemberList(Map<String, Object> paramMap) throws Exception;
+	
+	public int insertMember(Member member) throws Exception;
+	
+	public Member selectMemberByPK(String mb_no) throws Exception;
+
+	public Member selectMemberById(String fb_id) throws Exception;
+	
+	public int deleteMember(String mb_no) throws Exception;
+	
+	
+	
+	
 }
