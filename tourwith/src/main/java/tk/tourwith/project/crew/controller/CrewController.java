b4cccf0823db98c1b36b5c323c12b8188eea1562
Unknown <tivmtlarbdud@gmail.com>
2018-02-14 13:53:09 +0900

    crew controller 수정중

--- tourwith/src/main/java/tk/tourwith/project/crew/controller/CrewController.java
+++ tourwith/src/main/java/tk/tourwith/project/crew/controller/CrewController.java
@@ -10,17 +10,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import tk.tourwith.project.crew.model.Crew;
 import tk.tourwith.project.crew.service.impl.CrewServiceImpl;
 
 @Controller
+@RequestMapping("/crew")
 public class CrewController {
 	
 	@Autowired
 	CrewServiceImpl crewService;	
 	
-	@RequestMapping("/crew/{category}")
+	@RequestMapping("/{category}")
 	public String getCrewList(@PathVariable String category, Model model) throws Exception {
 		
 		Map<String, Object> paramMap = new HashMap<>();
@@ -32,7 +35,7 @@ public class CrewController {
 		return "crew/crewList";
 	}
 	
-	@RequestMapping("/crew/page/{cr_no}")
+	@RequestMapping("/page/{cr_no}")
 	public String crewView(Model model
 						  ,@PathVariable(value="cr_no", required=true) String cr_no) throws Exception {
 		Crew crew = null;
@@ -44,5 +47,25 @@ public class CrewController {
 		return "crew/crewView";
 	}
 	
-
+	@RequestMapping("/CrewForm")
+	public String crewForm(@RequestParam(value="cr_no", required=false, defaultValue="0") String cr_no,
+			Model model) throws Exception{
+		
+		Crew crew = new Crew();
+		
+		model.addAttribute("crew", crew);
+		
+		return "crew/crewForm";
+	}
+	
+	@RequestMapping(value="/crewInsert", method=RequestMethod.POST)
+	public String insertCrew(Crew crew,
+							 Model model) throws Exception{
+		
+		String updCnt = crewService.insertCrew(crew);
+		
+		
+		
+		return "crew/crewForm";
+	}
 }
